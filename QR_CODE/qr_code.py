b3088012c44170c027b45984c5a3df75e7712a04
Ruquiya Anjum <81841685+ruquiyaanjum@users.noreply.github.com>
2024-01-29 14:10:44 +0530

    Update qr_code.py

--- QR_CODE/qr_code.py
+++ QR_CODE/qr_code.py
@@ -1,1 +1,6 @@
-
+""" QR CODE USING PYTHON """
+import pyqrcode
+from pyqrcode import QRCode
+s="https://www.linkedin.com/in/ruquiya-anjum-4544431bb/"
+url=pyqrcode.create(s)
+url.svg("my linkedin.svg",scale=8)
