429262a00434397504f45c9640fa65b30510873c
Ruquiya Anjum <81841685+ruquiyaanjum@users.noreply.github.com>
2024-01-29 15:30:08 +0530

    Create tcl_files.tcl

---
+++ TCL_PYTHON_REGRESSION/tcl_files.tcl
@@ -0,0 +1,31 @@
+#TCL CODE FOR READING A MULTIPLE FILES FROM LOCAL DIRECTORY
+# Define the folder containing CSV files
+set folder_path "/home/cdac/data"
+
+# Get a list of all CSV files in the folder
+set csv_files [glob -nocomplain "${folder_path}/*.csv"]
+
+# Iterate over each CSV file
+foreach file_name $csv_files {
+    # Open the CSV file for reading
+    set file [open $file_name "r"]
+    puts "Contents of $file_name:"
+    
+    # Read and process each line of the file
+    while {[gets $file line] != -1} {
+        # Split the line into fields
+        set fields [split $line ","]
+        
+        # Print each field
+        foreach field $fields {
+            puts $field
+        }
+    }
+    
+    puts "" ;# Print an empty line to separate files
+    
+    # Close the file
+    close $file
+}
+
+
